592a3583919034e1d7f61aabe0c8af4f7713a336
Brady Walsh <walshb8@wit.edu>
2018-03-29 15:09:43 -0400

    Update queries with dummy data

--- database/headCountApp.sqlite.sql
+++ database/headCountApp.sqlite.sql
@@ -51,3 +51,31 @@ CREATE TABLE [Admins] (
    Creating Default Admin User
 ********************************************/
 INSERT INTO [Admins] ([Username], [Password]) VALUES ('root', 'password');
+
+INSERT INTO [Clients] ([UserID]) VALUES ('KVd3Tu');
+
+/*******************************************
+   Creating dummy data
+********************************************/
+INSERT INTO [Timeslots] ([TimeslotID],[Timeslot]) VALUES (1,'2:00 PM');
+INSERT INTO [Timeslots] ([TimeslotID],[Timeslot]) VALUES (2,'1:00 PM');
+INSERT INTO [Timeslots] ([TimeslotID],[Timeslot]) VALUES (3,'3:00 PM');
+INSERT INTO [Timeslots] ([TimeslotID],[Timeslot]) VALUES (4,'12:00 AM');
+INSERT INTO [Timeslots] ([TimeslotID],[Timeslot]) VALUES (5,'11:00 AM');
+
+INSERT INTO [Rooms] ([RoomID],[Room],[Capacity]) VALUES (1,'234',80);
+INSERT INTO [Rooms] ([RoomID],[Room],[Capacity]) VALUES (2,'123',180);
+INSERT INTO [Rooms] ([RoomID],[Room],[Capacity]) VALUES (3,'311',100);
+INSERT INTO [Rooms] ([RoomID],[Room],[Capacity]) VALUES (4,'122',30);
+INSERT INTO [Rooms] ([RoomID],[Room],[Capacity]) VALUES (5,'222',60);
+
+INSERT INTO [Forms] ([FormID],[RoomID],[TimeslotID],[HeadcountType],[HeadcountCount],[UserID]) 
+  VALUES (1,2,4,'Beginning',120,'KVd3Tu');
+INSERT INTO [Forms] ([FormID],[RoomID],[TimeslotID],[HeadcountType],[HeadcountCount],[UserID]) 
+  VALUES (2,1,2,'Middle',60,'KVd3Tu');
+INSERT INTO [Forms] ([FormID],[RoomID],[TimeslotID],[HeadcountType],[HeadcountCount],[UserID]) 
+  VALUES (3,3,1,'End',90,'KVd3Tu');
+INSERT INTO [Forms] ([FormID],[RoomID],[TimeslotID],[HeadcountType],[HeadcountCount],[UserID]) 
+  VALUES (4,5,3,'Beginning',50,'KVd3Tu');
+INSERT INTO [Forms] ([FormID],[RoomID],[TimeslotID],[HeadcountType],[HeadcountCount],[UserID]) 
+  VALUES (5,4,5,'Middle',25,'KVd3Tu');
