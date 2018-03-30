46ad8133657b2a5488fe21ebb40227d19a6c27de
Ford Polia <poliaf@wit.edu>
2018-03-30 10:55:24 -0400

    Removed login.php, index.php is now the correct login page

--- admin/index.php
+++ admin/index.php
@@ -1,3 +1,75 @@
+<?php
+// Include config file
+require_once 'config.php';
+
+// Define variables and initialize with empty values
+$username = "";
+$password = "";
+$username_err = "";
+$password_err = "";
+
+// Processing form data when form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Check if username is empty
+    if (empty(trim($_POST["username"]))) {
+        $username_err = 'Please enter username.';
+    } else {
+        $username = trim($_POST["username"]);
+    }
+
+    // Check if password is empty
+    if (empty(trim($_POST['password']))) {
+        $password_err = 'Please enter your password.';
+    } else {
+        $password = trim($_POST['password']);
+    }
+
+    // Validate credentials
+    if (empty($username_err) && empty($password_err)) {
+        // Prepare a select statement
+        $sql = "SELECT username, password FROM Admins WHERE username = :username";
+
+        if ($stmt = $phpDataObject->prepare($sql)) {
+            // Bind variables to the prepared statement as parameters
+            $stmt->bindParam(':username', $param_username, PDO::PARAM_STR);
+
+            // Set parameters
+            $param_username = trim($_POST["username"]);
+
+            // Attempt to execute the prepared statement
+            if ($stmt->execute()) {
+                // Check if username exists, if yes then verify password
+                if ($stmt->rowCount() == 1) {
+                    if ($row = $stmt->fetch()) {
+                        $hashed_password = $row['password'];
+                        if (password_verify($password, $hashed_password)) {
+                            /* Password is correct, so start a new session and
+                            save the username to the session */
+                            session_start();
+                            $_SESSION['username'] = $username;
+                            header("location: home.php");
+                        } else {
+                            // Display an error message if password is not valid
+                            $password_err = 'The password you entered was not valid.';
+                        }
+                    }
+                } else {
+                    // Display an error message if username doesn't exist
+                    $username_err = 'No account found with that username.';
+                }
+            } else {
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
+
+        // Close statement
+        unset($stmt);
+    }
+
+    // Close connection
+    unset($phpDataObject);
+}
+?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -20,20 +92,29 @@
 <div class="container">
     <div class="py-5 text-center">
         <h2>Login - Administrator Portal</h2>
-        <form action="" method="post">
-            <label for="usr">Name:</label>
-            <center>
-                <input type="text" class="form-control" id="usr" name="username" style="width: 400px;">
-            </center>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+
+            <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+                <label for="usr">Name:</label>
+                <center>
+                    <input type="text" class="form-control" name="username" id="usr" value="<?php echo $username; ?>"
+                           style="width: 400px;">
+                    <span class="help-block"><?php echo $username_err; ?></span>
+                </center>
+            </div>
             <br>
-            <label for="pwd">Password:</label>
-            <center>
-                <input type="password" class="form-control" id="pwd" name="password" style="width: 400px;">
-            </center>
+            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+                <label for="pwd">Password:</label>
+                <center>
+                    <input type="password" class="form-control" name="password" id="pwd" style="width: 400px;">
+                    <span class="help-block"><?php echo $password_err; ?></span>
+                </center>
+            </div>
             <br>
-            <input class="btn btn-outline-primary" name="submit" type="submit" value="Sign In">
-            <br>
-            <span><?php echo $error; ?></span>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Sign In">
+            </div>
+            <br/><br/>
         </form>
 
         <br><br>
