0b2b4d60eea8f6e72c33fc25c198caa93161693a
Ford Polia <poliaf@wit.edu>
2018-04-02 17:23:41 -0400

    Put working login back

--- admin/index.php
+++ admin/index.php
@@ -1,74 +1,87 @@
+<?php
+// Initialize the session
+session_start();
+// If session variable is not set it will redirect to login page
+if (!isset($_SESSION['username']) || empty($_SESSION['username'])) {
+    header("location: login.php");
+    exit;
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-  <meta charset="utf-8" />
-  <title>HeadCountApp</title>
-  <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.css">
-  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.1/umd/popper.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.js"></script>
+    <meta charset="utf-8"/>
+    <title>HeadCountApp</title>
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.1/umd/popper.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.js"></script>
 </head>
 
 <body>
-  <div class="container">
+<div class="container">
     <div class="row">
-      <div class="col-sm-12 text-center">
-        <br />
-        <div class="btn-group">
-          <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Users
-          </button>
-          <div class="dropdown-menu">
-            <a class="dropdown-item" href="user/viewUser.php">View Users</a>
-            <a class="dropdown-item" href="user/addUser.php">Add Users</a>
-            <a class="dropdown-item" href="user/modifyUser.php">Modify Users</a>
-            <a class="dropdown-item" href="user/deleteUser.php">Delete Users</a>
-          </div>
+        <div class="col-sm-12 text-center">
+            <br/>
+            <div class="btn-group">
+                <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false">
+                    Users
+                </button>
+                <div class="dropdown-menu">
+                    <a class="dropdown-item" href="user/viewUser.php">View Users</a>
+                    <a class="dropdown-item" href="user/addUser.php">Add Users</a>
+                    <a class="dropdown-item" href="user/modifyUser.php">Modify Users</a>
+                    <a class="dropdown-item" href="user/deleteUser.php">Delete Users</a>
+                </div>
+            </div>
+            <div class="btn-group">
+                <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false">
+                    Rooms
+                </button>
+                <div class="dropdown-menu">
+                    <a class="dropdown-item" href="room/viewRoom.php">View Rooms</a>
+                    <a class="dropdown-item" href="room/addRoom.php">Add Rooms</a>
+                    <a class="dropdown-item" href="room/modifyRoom.php">Modify Rooms</a>
+                    <a class="dropdown-item" href="room/deleteRoom.php">Delete Rooms</a>
+                </div>
+            </div>
+            <div class="btn-group">
+                <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false">
+                    Timeslots
+                </button>
+                <div class="dropdown-menu">
+                    <a class="dropdown-item" href="timeslot/viewTimeslot.php">View Timeslots</a>
+                    <a class="dropdown-item" href="timeslot/addTimeslot.php">Add Timeslots</a>
+                    <a class="dropdown-item" href="timeslot/modifyTimeslot.php">Modify Timeslots</a>
+                    <a class="dropdown-item" href="timeslot/deleteTimeslot.php">Delete Timeslots</a>
+                </div>
+            </div>
+            <div class="btn-group">
+                <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false">
+                    Forms
+                </button>
+                <div class="dropdown-menu">
+                    <a class="dropdown-item" href="form/addForm.php">Add Forms</a>
+                    <a class="dropdown-item" href="form/modifyForm.php">Modify Forms</a>
+                    <a class="dropdown-item" href="form/deleteForm.php">Delete Forms</a>
+                </div>
+            </div>
+            <br/>
+            <br/>
+            <div>
+                DISPLAY FORM DATA HERE USING PHP ECHO IN TABLE
+            </div>
+            <br/>
+            <a href="logout.php" class="btn btn-secondary">Logout</a>
         </div>
-        <div class="btn-group">
-          <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Rooms
-          </button>
-          <div class="dropdown-menu">
-            <a class="dropdown-item" href="room/viewRoom.php">View Rooms</a>
-            <a class="dropdown-item" href="room/addRoom.php">Add Rooms</a>
-            <a class="dropdown-item" href="room/modifyRoom.php">Modify Rooms</a>
-            <a class="dropdown-item" href="room/deleteRoom.php">Delete Rooms</a>
-          </div>
-        </div>
-        <div class="btn-group">
-          <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Timeslots
-          </button>
-          <div class="dropdown-menu">
-            <a class="dropdown-item" href="timeslot/viewTimeslot.php">View Timeslots</a>
-            <a class="dropdown-item" href="timeslot/addTimeslot.php">Add Timeslots</a>
-            <a class="dropdown-item" href="timeslot/modifyTimeslot.php">Modify Timeslots</a>
-            <a class="dropdown-item" href="timeslot/deleteTimeslot.php">Delete Timeslots</a>
-          </div>
-        </div>
-        <div class="btn-group">
-          <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Forms
-          </button>
-          <div class="dropdown-menu">
-            <a class="dropdown-item" href="form/addForm.php">Add Forms</a>
-            <a class="dropdown-item" href="form/modifyForm.php">Modify Forms</a>
-            <a class="dropdown-item" href="form/deleteForm.php">Delete Forms</a>
-          </div>
-        </div>
-        <br />
-        <br />
-        <div>
-          DISPLAY FORM DATA HERE USING PHP ECHO IN TABLE
-        </div>
-        <br />
-        <a href="logout.php" class="btn btn-secondary">Logout</a>
-      </div>
     </div>
-  </div>
+</div>
 </body>
 
 </html>
--- admin/login.php
+++ admin/login.php
@@ -1,36 +1,126 @@
-<!DOCTYPE html>
+<?php
+// Include config file
+require_once 'dbconfig.php';
+
+// Define variables and initialize with empty values
+$username = "";
+$password = "";
+$username_err = "";
+$password_err = "";
+
+// Processing form data when form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Check if username is empty
+    if (empty(trim($_POST["username"]))) {
+        $username_err = 'Please enter username.';
+    } else {
+        $username = trim($_POST["username"]);
+    }
+
+    // Check if password is empty
+    if (empty(trim($_POST['password']))) {
+        $password_err = 'Please enter your password.';
+    } else {
+        $password = trim($_POST['password']);
+    }
+
+    // Validate credentials
+    if (empty($username_err) && empty($password_err)) {
+        // Prepare a select statement
+        $sql = "SELECT username, password FROM Admins WHERE username = :username";
+
+        if ($stmt = $databaseConnection->prepare($sql)) {
+            // Bind variables to the prepared statement as parameters
+            $stmt->bindParam(':username', $param_username, PDO::PARAM_STR);
+
+            // Set parameters
+            $param_username = trim($_POST["username"]);
+
+            // Attempt to execute the prepared statement
+            if ($stmt->execute()) {
+                // Check if username exists, if yes then verify password
+                if ($stmt->rowCount() == 1) {
+                    if ($row = $stmt->fetch()) {
+                        $hashed_password = $row['password'];
+                        if (password_verify($password, $hashed_password)) {
+                            /* Password is correct, so start a new session and
+                            save the username to the session */
+                            session_start();
+                            $_SESSION['username'] = $username;
+                            header("location: index.php");
+                        } else {
+                            // Display an error message if password is not valid
+                            $password_err = 'The password you entered was not valid.';
+                        }
+                    }
+                } else {
+                    // Display an error message if username doesn't exist
+                    $username_err = 'No account found with that username.';
+                }
+            } else {
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
+
+        // Close statement
+        unset($stmt);
+    }
+
+    // Close connection
+    unset($databaseConnection);
+}
+?>
+<!doctype html>
 <html lang="en">
-
 <head>
-	<meta charset="utf-8" />
-	<title>HeadCountApp</title>
-	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.css">
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Admin Login Portal</title> <!-- change to what you want the tab's title to be -->
+
 </head>
-
 <body>
-	<div class="container">
-		<div class="row">
-			<div class="col-sm-4"></div>
-			<div class="col-sm-4 text-center">
-				<h1>Admin Login</h1>
-				<form action="home.php" method="post">
-					<div class="form-group">
-						<label>Username</label>
-						<input type="text" id="username" name="username" class="form-control">
-					</div>
-					<div class="form-group">
-						<label>Password</label>
-						<input type="password" id="password" name="password" class="form-control">
-					</div>
-					<input type="submit" value="Login" class="btn btn-primary" />
-				</form>
-				<br />
-				<a href="../client">Volunteers Click Here</a>
-			</div>
-			<div class="col-sm-4"></div>
-		</div>
-	</div>
-</body>
+<div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom box-shadow">
+    <h5 class="my-0 mr-md-auto font-weight-normal">Boston Code Camp</h5>
+</div>
 
+<div class="container">
+    <div class="py-5 text-center">
+        <h2>Login - Administrator Portal</h2>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+
+            <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+                <label for="usr">Name:</label>
+                <center>
+                    <input type="text" class="form-control" name="username" id="usr" value="<?php echo $username; ?>"
+                           style="width: 400px;">
+                    <span class="help-block"><?php echo $username_err; ?></span>
+                </center>
+            </div>
+            <br>
+            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+                <label for="pwd">Password:</label>
+                <center>
+                    <input type="password" class="form-control" name="password" id="pwd" style="width: 400px;">
+                    <span class="help-block"><?php echo $password_err; ?></span>
+                </center>
+            </div>
+            <br>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Sign In">
+            </div>
+            <br/><br/>
+        </form>
+
+        <br><br>
+        <h5> Not an administrator? Sign in to the Volunteer Portal:</h5>
+        <a class="btn btn-outline-primary" href="../client/index.php">Volunteer Sign In</a>
+    </div>
+</div>
+</body>
 </html>
