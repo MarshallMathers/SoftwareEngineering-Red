eee3e11184c9e6712193e273ca0cd145057f3086
Timothy Boss <bosst@wit.edu>
2018-04-03 02:52:32 -0400

    update

--- client/index.php
+++ client/index.php
@@ -61,33 +61,35 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 		$sqlDuplicate = "SELECT * FROM Forms WHERE RoomID = '$roomID' AND TimeslotID = '$timeslotID' AND HeadcountType = '$headCountType'";
 		$resultDuplicate = mysqli_query($link, $sqlDuplicate);
 		if (mysqli_num_rows($resultDuplicate) != 0){
-			echo "<script>if(confirm('This form already exists. Are you sure you want to override it?')){}else{window.location = 'index.php';}</script>";
+			if(echo "<script>confirm('This form already exists. Are you sure you want to override it?')</script>")
+			{
+				// Prepare a select statement
+				$sql = "INSERT INTO Forms (RoomID, TimeslotID, HeadcountType, HeadcountCount, UserID) VALUES (?, ?, ?, ?, ?)";
+
+				if ($stmt = mysqli_prepare($link, $sql)) {
+					// Bind variables to the prepared statement as parameters
+					mysqli_stmt_bind_param($stmt, "sssss", $param_roomID, $param_timeslotID, $param_headCountType, $param_headCount, $param_userID);
+					
+					// Set parameters
+					$param_roomID = $roomID;
+					$param_timeslotID = $timeslotID;
+					$param_headCountType = $headCountType;
+					$param_headCount = $headCount;
+					$param_userID = $_SESSION["userID"];
+		
+					// Attempt to execute the prepared statement
+					if (mysqli_stmt_execute($stmt)) {
+						echo "<script>alert('Form successfully submitted!');window.location = 'index.php';</script>";
+					} else {
+						echo "<script>alert('Oops! Something went wrong. Please try again later.');window.location = 'index.php';</script>";
+					}
+				}
+		
+				// Close statement
+				mysqli_stmt_close($stmt);
+			}else{
+				echo "<script>window.location = 'index.php';}</script>";
 		}
-
-        // Prepare a select statement
-        $sql = "INSERT INTO Forms (RoomID, TimeslotID, HeadcountType, HeadcountCount, UserID) VALUES (?, ?, ?, ?, ?)";
-
-        if ($stmt = mysqli_prepare($link, $sql)) {
-            // Bind variables to the prepared statement as parameters
-			mysqli_stmt_bind_param($stmt, "sssss", $param_roomID, $param_timeslotID, $param_headCountType, $param_headCount, $param_userID);
-			
-			// Set parameters
-			$param_roomID = $roomID;
-			$param_timeslotID = $timeslotID;
-			$param_headCountType = $headCountType;
-			$param_headCount = $headCount;
-			$param_userID = $_SESSION["userID"];
-
-            // Attempt to execute the prepared statement
-            if (mysqli_stmt_execute($stmt)) {
-				echo "<script>alert('Form successfully submitted!');window.location = 'index.php';</script>";
-            } else {
-                echo "<script>alert('Oops! Something went wrong. Please try again later.');window.location = 'index.php';</script>";
-            }
-        }
-
-        // Close statement
-        mysqli_stmt_close($stmt);
     }
 
     // Close connection
