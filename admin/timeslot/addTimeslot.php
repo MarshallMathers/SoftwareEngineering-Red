10bc5d21ebbd63b85f223c574ac813bb668d8cc4
Holmesg2 <gilesholmespwns@gmail.com>
2018-04-03 16:04:56 -0400

    added session start

--- admin/timeslot/addTimeslot.php
+++ admin/timeslot/addTimeslot.php
@@ -1,4 +1,13 @@
 <?php
+
+// Initialize the session
+session_start();
+// If session variable is not set it will redirect to login page
+if (!isset($_SESSION["username"]) || empty($_SESSION["username"])) {
+    header("location: login.php");
+    exit;
+}
+
 include '../../dbconfig.php';
 
 $sql1 = "SELECT Timeslot FROM timeslots";
