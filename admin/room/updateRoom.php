63dfe5e377530cdabfa554cfe1511c4d70e797f1
Holmesg2 <gilesholmespwns@gmail.com>
2018-04-03 15:57:32 -0400

    added session start

--- admin/room/updateRoom.php
+++ admin/room/updateRoom.php
@@ -1,4 +1,11 @@
 <?php
+// Initialize the session
+session_start();
+// If session variable is not set it will redirect to login page
+if (!isset($_SESSION["username"]) || empty($_SESSION["username"])) {
+    header("location: login.php");
+    exit;
+}
 include '../../dbconfig.php';
 
 if (isset($_POST['room_ID'])){
