0d02586d15d7a0aca65a6f81cff2f270f5ff30c2
Timothy Boss <bosst@wit.edu>
2018-04-03 00:46:29 -0400

    update

--- client/index.php
+++ client/index.php
@@ -1,7 +1,7 @@
 <?php
 	// Initialize the session
 	session_start();
-	if (!isset($_SESSION["user_ID"]) || empty($_SESSION["user_ID"])) {
+	if (!isset($_SESSION["userID"]) || empty($_SESSION["userID"])) {
     	header("location: login.php");
   		exit;
 	}
@@ -39,7 +39,7 @@
     <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom box-shadow">
     	<h5 class="my-0 mr-md-auto font-weight-normal," background-color:DodgerBlue;> Boston Code Camp </h5> 
     	<nav class="my-2 my-md-0 mr-md-3">
-        	<a class="p-10 color: blue"><?php echo $_SESSION["user_ID"]; ?></a> 
+        	<a class="p-10 color: blue"><?php echo $_SESSION["userID"]; ?></a> 
         	<p style="color:Blue;"> </p> 
           <!--this will be the name of the logged in admin -->
         </nav>
