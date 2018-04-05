02d57d6895cd2652cf2b10b18fa81e249de95986
Ford Polia <poliaf@wit.edu>
2018-04-05 09:55:43 -0400

    Updated to version with max attribute changed to maxlength

--- admin/index.php
+++ admin/index.php
@@ -80,8 +80,8 @@ include "../dbconfig.php";
                         <table class="table table-bordered">
                             <?php
                                 $sql = "
-                                SELECT Rooms.RoomID, Rooms.Room, Timeslots.Timeslot, Forms.HeadcountType, Forms.HeadcountCount, Forms.UserID, Forms.Timestamp FROM Forms LEFT JOIN Rooms ON Forms.RoomID = Rooms.RoomID LEFT JOIN Timeslots ON Forms.TimeslotID = Timeslots.TimeslotID 
-                                ORDER BY RoomID, HeadcountType='End', HeadcountType='Middle', HeadcountType='Beginning'
+                                SELECT Rooms.RoomID, Rooms.Room, Timeslots.TimeslotID, Timeslots.Timeslot, Forms.HeadcountType, Forms.HeadcountCount, Forms.UserID, Forms.Timestamp FROM Forms LEFT JOIN Rooms ON Forms.RoomID = Rooms.RoomID LEFT JOIN Timeslots ON Forms.TimeslotID = Timeslots.TimeslotID 
+                                ORDER BY RoomID, TimeslotID, HeadcountType='End', HeadcountType='Middle', HeadcountType='Beginning'
                                 ";
 
                                 $result = mysqli_query($link, $sql);
